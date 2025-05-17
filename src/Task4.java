b0cc7d44db6c292b0d8b6c820fc4cda787cca8c6
NataliaGumarova <n.gu.47943@gmail.com>
2025-05-18 00:29:51 +0400

    Задача 4

---
+++ src/Task4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = scan.nextLine();
+        String word = text.replace(",", "").
+                replace(".", " ").
+                replace("!", " ").
+                replace("?", " ").
+                replace("(", " ").
+                replace(")", " ").
+                replace(";", " ").
+                replace(":", " ").
+                replace("-", " ").
+                replace("_", " ").
+                replace("\"", " ");
+        String[] words = word.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String regular = "^[aAeEyYuUiIoO][a-z]{1,189819}[qQwWrRtTpPsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM]$";
+            // длина самого длинного слова в мире - 189819 букв
+            boolean y = words[i].trim().matches(regular);
+                if (y) {
+                 System.out.println(words[i].trim());
+                }
+            }
+        }
+    }
