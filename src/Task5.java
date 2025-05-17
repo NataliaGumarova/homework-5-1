372df7872ce2cb0e932c20cbb63f6bcb6fe920f1
NataliaGumarova <n.gu.47943@gmail.com>
2025-05-18 00:42:19 +0400

    Задача 5

---
+++ src/Task5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = scan.nextLine();
+        String word = text.replace(",", "").
+                replace(".", " ").
+                replace("!", " ").
+                replace("?", " ").
+                replace("(", " ").
+                replace(")", " ").
+                replace(";", " ").
+                replace(":", " ").
+                replace("-", " ").
+                replace("_", " ").
+                replace("\"", " ");
+        String[] words = word.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            char[] w = words[i].trim().toCharArray();
+            if (w.length % 2 == 0) {
+                System.out.println(words[i].trim());
+            }
+        }
+    }
+}
